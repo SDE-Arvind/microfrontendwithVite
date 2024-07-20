46eafc0870083ed6c9ee66a108cc44691db607e0
Arvind <arvind.rathour06@gmail.com>
2024-07-20 10:48:28 +0530

    added root config using Vite and React to show header

---
+++ vite-root-config/src/header/Header.tsx
@@ -0,0 +1,23 @@
+import './styles.css';
+
+import React, { useEffect, useState } from 'react';
+
+function Header() {
+
+  return (
+    <div id="header">
+      <div className='actionBar'>
+      <label className="label">This is the header component from Root Project</label>
+        <nav>
+          <ul id='menu'>
+            <li><a id="menu" href="/profiles">Customers</a></li>
+            {/* <li><a href="/address" id="menu">Address</a></li> */}
+            {/* <li><a href="/orders" id="menu">Orders</a></li> */}
+          </ul>
+        </nav>
+      </div>
+     
+    </div>
+  );
+}
+export default Header;
